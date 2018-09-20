403efc43b371c4ca737ecdee10f9b5e1e8bf2b92
Ken Long <long27km@uw.edu>
2018-09-20 16:03:37 -0700

    feature: #32, addresses #56

--- api/src/main/java/org/openmrs/module/mentalhealth/elements/NumberInputElement.java
+++ api/src/main/java/org/openmrs/module/mentalhealth/elements/NumberInputElement.java
@@ -1,7 +1,9 @@
 package org.openmrs.module.mentalhealth.elements;
 
+import java.util.Locale;
 import java.util.Map;
 
+import org.openmrs.api.context.Context;
 import org.openmrs.module.htmlformentry.FormEntrySession;
 import org.w3c.dom.Node;
 
@@ -14,14 +16,54 @@ public class NumberInputElement extends InputElement {
 		// TODO check m_parameters.get("step") (else default to html5 spec default step of 1) for step and determine the precision to provide for the input value attr in view and edit modes
 	}
 
+	private int getPrecision(String formattedString, int currentPrecision, String decimalSeparator) {
+		
+		if(formattedString != null && !formattedString.isEmpty()) {
+			int index = formattedString.indexOf(decimalSeparator);
+			
+			if(index >= 0) {
+				
+				int precision = formattedString.length() - index - 1;
+				
+				if(precision > currentPrecision) {
+					return precision;
+				}
+				
+			}
+		}
+		
+		return currentPrecision;
+	}
+	
 	@Override
 	public String derivedClassSpecializeHTMLEditProcessing()
 	{
 		String result = null;
 		
 		if(m_prevObs!=null) {
-			//only support integer values for now
-			result = String.valueOf(m_prevObs.getValueNumeric().intValue());
+			String []formattedStrings = new String[] {
+										m_parameters.get("step"), 
+										m_parameters.get("min"),
+										m_parameters.get("max")
+									};
+			
+			Integer precision = 0;
+			
+			String decimalSeparator = ".";
+			//?Context.getLocale() == new Locale("en", "US") ? "." : ",";?
+			
+			for(String formattedString: formattedStrings) {
+				precision = getPrecision(formattedString, precision, decimalSeparator);
+			}
+			
+			String precisionSpecifier = "%."+String.valueOf(precision)+"f";
+			
+			try {
+				result = String.format(precisionSpecifier, m_prevObs.getValueNumeric());
+			}catch(Exception e) {
+				e.printStackTrace();
+				result = null;
+			}
 		}
 		
 		return result;
